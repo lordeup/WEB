3d976e7fb9646da7f21e08deb6bf889cf4649737
Andrey Kalanov <lordeup@gmail.com>
2019-05-30 16:44:25 +0300

    Добавлено второе задание в лабораторной работе 8

--- lw8/create_fill_db_lw8.sql
+++ lw8/create_fill_db_lw8.sql
@@ -20,11 +20,10 @@ GO
 
 DROP TABLE IF EXISTS dbo.track_downloads;
 CREATE TABLE dbo.track_downloads 
-( 
-	download_id BIGINT PRIMARY KEY IDENTITY NOT NULL, 
-    track_id BIGINT NOT NULL, 
+   (download_id BIGINT PRIMARY KEY IDENTITY NOT NULL, 
+    track_id INT NOT NULL, 
     user_id BIGINT NOT NULL, 
-    download_time TIMESTAMP NOT NULL)
+    download_time DATETIME NOT NULL DEFAULT 0)
 GO
 
 
@@ -32,3 +31,19 @@ INSERT PC ([cpu], [memory], [hdd]) VALUES (1600, 2000, 500);
 INSERT PC ([cpu], [memory], [hdd]) VALUES (2400, 3000, 800);
 INSERT PC ([cpu], [memory], [hdd]) VALUES (3200, 3000, 1200);
 INSERT PC ([cpu], [memory], [hdd]) VALUES (2400, 2000, 500);
+
+
+TRUNCATE TABLE track_downloads;
+
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (99, 5, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (1, 8, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (20, 5, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (2, 5, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (7, 8, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (14, 5, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (15, 2, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (20, 4, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (1, 9, '20101119');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (19, 3, '20100830');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (11, 6, '20101121');
+INSERT track_downloads ([track_id], [user_id], [download_time]) VALUES (31, 13, '20100615');
--- lw8/queries_db_lw8.sql
+++ lw8/queries_db_lw8.sql
@@ -11,3 +11,13 @@ FROM PC;
 SELECT TOP 1 COUNT(hdd) AS count_min_hdd, hdd
 FROM PC
 GROUP BY hdd;
+
+SELECT track_downloads_subquery.download_count, COUNT(track_downloads_subquery.download_count) AS user_count 
+FROM
+(
+    SELECT user_id, COUNT(download_id) AS download_count 
+    FROM track_downloads
+	WHERE download_time = '20101119'
+    GROUP BY user_id
+) AS track_downloads_subquery
+GROUP BY track_downloads_subquery.download_count;
